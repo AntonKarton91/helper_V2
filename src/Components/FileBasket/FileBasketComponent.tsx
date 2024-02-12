9b40b9856603b7cb63ef246da16cd2839ba5b2fb
Киселев Антон Викторович <kiselev@deka.new>
2024-02-12 18:54:01 +0300

    t

--- src/Components/FileBasket/FileBasketComponent.tsx
+++ src/Components/FileBasket/FileBasketComponent.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 import {DetailedHTMLProps, HTMLAttributes, useState} from "react";
+import {useAppDispatch, useAppSelector} from "../../Store/hooks";
+import styles from "./fileBasket.module.scss"
+import {Avatar} from "@mui/material";
 
 const { ipcRenderer } = window.require('electron');
+const path = window.require('path');
 
 export interface FileBasketProps {
 
@@ -9,7 +13,11 @@ export interface FileBasketProps {
 
 
 const FileBasketComponent = ({}: FileBasketProps): React.ReactElement => {
-    const [path, setPath] = useState(__dirname)
+    const [ basketTitle, setBasketTitle ] = useState<string>("Перетащите файлы сюда для загрузки")
+    const [ basketIcon, setBasketIcon ] = useState<boolean>(false)
+    const { sheetList, appStatus } = useAppSelector(state => state.main)
+    const dispatch = useAppDispatch()
+
     const handleDrop = (event:any) => {
         event.preventDefault();
         event.stopPropagation();
@@ -17,6 +25,9 @@ const FileBasketComponent = ({}: FileBasketProps): React.ReactElement => {
         const files = Array.from(event.dataTransfer.files);
         // @ts-ignore
         const filePaths = files.map(file => file.path);
+        const splittedPAth = filePaths[0].split("\\")
+        setBasketTitle(splittedPAth[splittedPAth.length-3])
+        setBasketIcon(true)
         // if (files.length > 0) {
         //     ipcRenderer.send('file-drop', files);
         // }
@@ -33,9 +44,12 @@ const FileBasketComponent = ({}: FileBasketProps): React.ReactElement => {
             <div
                 onDrop={handleDrop}
                 onDragOver={handleDragOver}
-                style={{ width: '100%', height: '200px', border: '2px dashed #ccc' }}
+                className={styles.basketContainer}
             >
-                <p style={{ textAlign: 'center', marginTop: '80px' }}>Перетащите файлы сюда для загрузки</p>
+                {
+                    basketIcon && <Avatar src="https://cdn1.iconfinder.com/data/icons/application-file-formats/128/microsoft-excel-512.png" />
+                }
+                <div>{ basketTitle }</div>
             </div>
         </div>
     );
